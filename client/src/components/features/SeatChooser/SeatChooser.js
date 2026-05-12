ffda28e5a7811e57d971d885abe4cb4cd15df6f7
Marcin Suder <marcin.code.github@gmail.com>
2026-05-13 01:39:26 +0200

    fix socket render

--- client/src/components/features/SeatChooser/SeatChooser.js
+++ client/src/components/features/SeatChooser/SeatChooser.js
@@ -33,7 +33,7 @@ const SeatChooser = ({ chosenDay, chosenSeat, updateSeat }) => {
     });
 
     return () => socket.disconnect();
-  }, );
+  }, [dispatch]);
   
   // useEffect(() => {
   //   dispatch(loadSeatsRequest());
